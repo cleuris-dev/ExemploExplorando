cb55595d578c196ed616ca587d8627a4b7c87620
“cleuris-dtic” <cleuris89@gmail.com>
2025-02-18 16:13:10 -0300

    encerra o modulo de propriedades metodos e construtores

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿
 using ExemploExplorando.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Cleuris";
-p1.Sobrenome = "Adão";
-p1.Idade = 35;
-p1.Apresentar(); 
+Pessoa p1 = new Pessoa(nome: "Cleuris", sobrenome: "Adão");
+
+Pessoa p2 = new Pessoa("Eduardo", "Queiroz");
+
+Curso cursoIngles = new Curso();
+cursoIngles.Nome = "Ingles";
+cursoIngles.Alunos = new List<Pessoa>();
+
+cursoIngles.AdicionarAluno(p1);
+cursoIngles.AdicionarAluno(p2);
+cursoIngles.ListarAlunos();
+
+
